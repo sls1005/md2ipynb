1733d3375bb077b36256f440c434f3d26fd15c65
sls1005 <sls1005@github.com>
2024-03-11 20:43:04 +0800

    Add many files

---
+++ README.md
@@ -0,0 +1,66 @@
+Here is a command line tool that converts a Markdown file into a Jupyter notebook file.
+
+### Example
+
+##### Input
+
+```markdown
+    # Example
+
+    Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+
+    ```python
+    s = "Lorem ipsum"
+    ```
+
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.
+
+    ```python
+    print(s)
+    ```
+```
+
+##### Result (Opened with [Jupyter Notebook](https://github.com/jupyter/notebook))
+
+![](./screenshots/screenshot1.png)
+
+### Requirements
+
++ Python 3.3 or later
+
+### Usage
+
+```$ python md2ipynb.py FILE.md```
+
+To create a code cell, use ``` in the input file to form a block. You should also provide the language identifier. (e.g. `python` for Python. See [this documentation](https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/creating-and-highlighting-code-blocks) for more information.) For example:
+
+```markdown
+    ```python
+    import os
+    ```
+```
+
+A code cell will only be created if the programming language of the block matches the language to be used in the notebook. The language can be set with `--language`, while the identifier to search for can be set with `--language-identifier`.
+The block has to be at the top (unindented) level. This documentation *incorrectly* indents the examples due to technical reason.
+
+This does not check the syntax however, so it also turns things like
+```markdown 
+    ```python
+    %pip install numpy
+    ```
+```
+into code cells (even if it isn't correct Python), as long as the identifier matches.
+
+Everything else (including a code block of a different programming language) in the input file will be placed into Markdown cells. When creating a Markdown cell, the leading blank lines (that contain only line breaks) will be removed. If a Markdown cell would contain only blank lines, it is not created. This prevents creating a large number of empty cells.
+
+### References
+
+* https://nbformat.readthedocs.io/en/latest/format_description.html
+
+* https://ecma-international.org/wp-content/uploads/ECMA-404_2nd_edition_december_2017.pdf
+
+* https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/creating-and-highlighting-code-blocks
+
+* https://daringfireball.net/projects/markdown/syntax
+
+* https://docs.python.org/3/library/re.html
---
+++ md2ipynb.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+# -*- coding: ASCII -*-
+import re
+from json import dumps as to_json
+from sys import stdout, stderr, argv
+from os.path import exists
+
+usage = '''
+usage: md2ipynb.py [options] <file>
+
+arguments:
+      <file>                                  The name of the input file (a Markdown file).
+
+options:
+      -h, --help                              Display this help message and exit.
+
+      -e, --encoding <encoding>               The encoding of the input file.
+
+      -o, --output <file>                     The name of the output file. Usually ends with '.ipynb'.
+
+      -k, --kernel <kernel>                   The kernel to be used by the created notebook. (default: 'python3')
+
+      -lang, --language <language>            The programming language. (default: 'python')
+
+      --language-identifier <identifier>      The identifier used in the input file for syntax highlighting. Default to the language name given with '-lang', or 'python' if none.
+
+      --overwrite                             Overwrite the output file if it exists.
+'''
+
+def convert(input_file_name, output_file_name, input_file_encoding = 'UTF-8', output_file_encoding = 'UTF-8', language = 'python', language_identifier = 'python', kernel = 'python3'):
+    code_block_start = re.compile("^(\\x20){0,3}```(\\s)*" + language_identifier + "(\\s)*$")
+    other_block_start = re.compile("^(\\x20){0,3}```")
+    block_end = re.compile("^(\\x20){0,3}```(\\s)*$")
+    in_code_block = False
+    in_other_block = False
+    in_markdown = False
+    at_start = True
+    at_start_of_code_block = False
+    metadata = {
+        'kernelspec': {
+            'name': kernel
+        },
+        'language_info': {
+            'name': language
+        }
+    }
+    input_file = open(input_file_name, 'r', encoding=input_file_encoding)
+    output_file = open(output_file_name, 'w', encoding=output_file_encoding)
+    output_file.write('{"nbformat":%d,"nbformat_minor":%d,"cells":[\n' % (4, 0))
+    for line in input_file:
+        if in_code_block:
+            if block_end.search(line): # != None
+                output_file.write('","outputs": []}')
+                in_code_block = False
+            else:
+                #assert line[-1] == '\n'
+                c = ''
+                if at_start_of_code_block:
+                    at_start_of_code_block = False
+                else:
+                    c = '\n'
+                output_file.write(to_json(c + line[:-1])[1:-1])
+        elif code_block_start.search(line):
+            if in_other_block:
+                continue
+            if in_markdown:
+                output_file.write('"}')
+                in_markdown = False
+            if at_start:
+                at_start = False
+            else:
+                output_file.write(',\n')
+            output_file.write('{"cell_type":"code","metadata":{},"source":"')
+            in_code_block = True
+            at_start_of_code_block = True
+        elif in_markdown:
+            if in_other_block:
+                if block_end.search(line):
+                    in_other_block = False
+            else:
+                if other_block_start.search(line):
+                    in_other_block = True
+            output_file.write(to_json(line)[1:-1])
+        else:
+            if line == '\n':
+                continue
+            elif at_start:
+                at_start = False
+            else:
+                output_file.write(',\n')
+            output_file.write('{"cell_type":"markdown","metadata":{},"source":"' + to_json(line)[1:-1])
+            in_markdown = True
+    if in_markdown:
+        output_file.write('"}')
+    elif in_code_block:
+        output_file.write('","outputs":[]}')
+    input_file.close()
+    output_file.write('],\n"metadata":' + to_json(metadata) + '}')
+    output_file.close()
+
+def main():
+    input_file_name = ''
+    output_file_name = ''
+    language = 'python'
+    language_identifier = 'python'
+    kernel = 'python3'
+    encoding = 'UTF-8'
+    overwrite = False
+    argc = len(argv)
+    k = argc - 1
+    i = 1
+    while i < argc:
+        a = argv[i]
+        if a in ('-h', '--help'):
+            stdout.write(usage)
+            exit()
+        elif a in ('-e', '--encoding'):
+            if i < k:
+                encoding = argv[i+1]
+                i += 2
+                continue
+            else:
+                stderr.write("[Error] Expected an encoding after %s.\n" % a)
+                exit(1)
+        elif a in ('-o', '--output'):
+            if i < k:
+                output_file_name = argv[i+1]
+                i += 2
+                continue
+            else:
+                stderr.write("[Error] Expected a file name after %s.\n" % a)
+                exit(1)
+        elif a in ('-k', '--kernel'):
+            if i < k:
+                kernel = argv[i+1]
+                i += 2
+                continue
+            else:
+                stderr.write("[Error] Expected a name after %s.\n" % a)
+                exit(1)
+        elif a in ('-lang', '--language'):
+            if i < k:
+                language = argv[i+1]
+                i += 2
+                continue
+            else:
+                stderr.write("[Error] Expected a language name after %s.\n" % a)
+                exit(1)
+        elif a == '--language-identifier':
+            if i < k:
+                language_identifier = argv[i+1]
+                i += 2
+                continue
+            else:
+                stderr.write("[Error] Expected a language identifier after %s.\n" % a)
+                exit(1)
+        elif a == '--overwrite':
+            overwrite = True
+        else:
+            input_file_name = a
+        i += 1
+
+    if input_file_name == '':
+        stdout.write(usage)
+        exit()
+    elif output_file_name == '':
+        if '.' in input_file_name:
+            i = input_file_name.rindex('.')
+            output_file_name = input_file_name[:i+1] + 'ipynb'
+        else:
+            output_file_name = input_file_name + '.ipynb'
+    if exists(output_file_name) and not overwrite:
+        stderr.write("[Error] '%s' exists. Use '--overwrite' to overwrite it.\n" % output_file_name)
+        exit(2)
+
+    convert(input_file_name, output_file_name, input_file_encoding=encoding, language=language, language_identifier=language_identifier, kernel=kernel)
+
+if __name__ == '__main__':
+    main()
